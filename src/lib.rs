a0e398949444c36b59d8cf4e8ec8611a1785e239
Nate Wilcox <nathan+dev@electriccoin.co>
2022-09-06 16:59:19 -0700

    First pass implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt;
+
+#[derive(Debug, derive_more::From)]
+pub struct ErrorVec<T>(Vec<T>);
+
+impl<A> FromIterator<A> for ErrorVec<A> {
+    fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = A>,
+    {
+        ErrorVec(iter.into_iter().collect())
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> fmt::Display for ErrorVec<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, e) in self.0.iter().enumerate() {
+            writeln!(f, "Error {}:\n{}\n", i, e)?;
+        }
+        Ok(())
     }
 }
