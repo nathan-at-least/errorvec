8ba2f578e22efd2e9eb6c4e178160bae1aad5bbc
Nate Wilcox <nathan+dev@electriccoin.co>
2022-09-07 19:22:07 -0700

    Flesh out `ErrorVec` documentation.

--- src/errorvec.rs
+++ src/errorvec.rs
@@ -1,6 +1,60 @@
 use std::fmt;
 
 /// A newtype wrapper around `Vec<E>` aimed at supporting multi-error scenarios.
+///
+/// # `Display`
+///
+/// [ErrorVec] implements [std::fmt::Display] by prepending each error's display with `[error K of N]`.
+///
+/// # `ResultIterator` usage
+///
+/// A common usage is via
+/// [ResultIterator::into_errorvec_result](crate::ResultIterator::into_errorvec_result) for gathering
+/// all errors in an [Iterator] over [Result] values.
+///
+/// # Empty `ErrorVec`
+///
+/// An [ErrorVec] containing no values (ie `ErrorVec::is_empty() == true`) typically does not
+/// represent an error, and the [ErrorVec::into_result] and [ErrorVec::into_result_with] are often
+/// useful in this case:
+///
+/// ```
+/// use errorvec::ErrorVec;
+///
+/// let ev: ErrorVec<()> = ErrorVec::default();
+/// assert!(ev.into_result().is_ok());
+/// ```
+///
+/// # Example - Gathering errors with `take_error` and `into_result_with`
+///
+/// For scenarios where [ResultIterator](crate::ResultIterator) isn't
+/// useful, the [ErrorVec::take_error] and [ErrorVec::into_result_with]
+/// methods may be useful.
+///
+/// ```
+/// use std::path::Path;
+/// use errorvec::ErrorVec;
+///
+/// /// Return the string contents of all the paths listed in the `manifest_file`, reporting all
+/// /// errors encountered.
+/// fn read_manifest_files(manifest_file: &Path) -> Result<Vec<String>, ErrorVec<std::io::Error>> {
+///     use std::fs::read_to_string;
+///
+///     let mut contents = vec![];
+///     let mut errs = ErrorVec::default();
+///
+///     if let Some(manifest) = errs.take_error(read_to_string(manifest_file)) {
+///         for line in manifest.lines() {
+///             let path = &Path::new(line.trim_end());
+///             if let Some(content) = errs.take_error(read_to_string(path)) {
+///                 contents.push(content)
+///             }
+///         }
+///     }
+///
+///     errs.into_result_with(contents)
+/// }
+/// ```
 #[derive(Debug, derive_more::From, derive_more::Into)]
 pub struct ErrorVec<E>(Vec<E>);
 
